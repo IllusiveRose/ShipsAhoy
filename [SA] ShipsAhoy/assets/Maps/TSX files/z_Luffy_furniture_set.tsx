3043f9e8729a3c2a2535d0f3ef4c09b915fd1a5e
IllusiveRose <rachel@illusiverose.com>
2024-09-04 00:48:14 -0400

    Removes all extraneous files

---
+++ [SA] ShipsAhoy/assets/Maps/TSX files/z_Luffy_furniture_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="z_Luffy_furniture_set" tilewidth="16" tileheight="16" tilecount="208" columns="13">
+ <image source="Luffy_furniture_set.png" width="208" height="265"/>
+</tileset>
