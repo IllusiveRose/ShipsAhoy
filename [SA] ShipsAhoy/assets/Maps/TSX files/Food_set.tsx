02af755dec69d7ec785469a27d28e522d632476d
IllusiveRose <rachel@illusiverose.com>
2024-09-09 18:58:03 -0400

    Adjust file structure.

--- [SA] ShipsAhoy/assets/Maps/TSX files/Food_set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Food_set" tilewidth="16" tileheight="16" tilecount="208" columns="13">
- <image source="Food_set.png" width="208" height="265"/>
-</tileset>
