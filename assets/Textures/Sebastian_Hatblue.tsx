baf3ce4462c48bdc87b394d4a574963b1327a39f
IllusiveRose <rachel@illusiverose.com>
2024-09-21 20:42:27 -0400

    Removed "Custom" from Portraits folder and adds customizable random outfits for all characters

---
+++ assets/Textures/Sebastian_Hatblue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Sebastian_Hatblue" tilewidth="64" tileheight="64" tilecount="30" columns="2">
+ <image source="../Portraits/Sebastian_Hatblue.png" width="128" height="960"/>
+</tileset>
