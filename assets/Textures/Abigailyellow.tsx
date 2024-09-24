67db0295da0938d247a500701db8868cd446676b
IllusiveRose <rachel@illusiverose.com>
2024-09-24 10:04:19 -0400

    Adds custom map but isn't working yet

---
+++ assets/Textures/Abigailyellow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Abigailyellow" tilewidth="16" tileheight="32" tilecount="88" columns="4">
+ <image source="../Characters/Abigailyellow.png" width="64" height="704"/>
+</tileset>
